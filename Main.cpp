3408314fb3f8f59b08205872333f1b3eaa16f2ae
RosuRobertAlexandru <73839891+RosuRobertAlexandru@users.noreply.github.com>
2022-12-01 22:28:47 +0200

    2nd idea , this directions seems correct

--- Main.cpp
+++ Main.cpp
@@ -16,16 +16,63 @@ private:
 	string seatRow = "";
 	string seatCode = "";
 public:
+	
+	void setTotalNoOfSeats(int totalNoOfSeats) {
+		this->totalNoOfSeats = totalNoOfSeats;
+	}
+	int getTotalNoOfSeats() {
+		return totalNoOfSeats;
+	}
+	void setNoOfSeatsBooked(int noOfSeatsBooked) {
+		this->noOfSeatsBooked = noOfSeatsBooked;
+	}
+	int getNoOfSeatsBooked() {
+		return noOfSeatsBooked;
+	}
+	void setSeatRow(string seatRow) {
+		this->seatRow = seatRow;
+	}
+	string getSeatRow() {
+		return seatRow;
+	}
+	void setSeatCode(string seatCode) {
+		this->seatCode = seatCode;
+	}
+	string getSeatCode() {
+		return seatCode;
+	}
 };
+
 class Event {
+private:
 	string date;
 	string dateFormat;
 	string name;
 	
-private:
+
 
 public:
+	
+	void setDate(string date) {
+		this->date = date;
+	}
+	string getDate() {
+		return date;
+	}
+	void setDateFormat(string dateFormat) {
+		this->dateFormat = dateFormat;
+	}
+	string getDateFormat() {
+		return dateFormat;
+	}
+	void setName(string name) {
+		this->name = name;
+	}
+	string getName() {
+		return name;
+	}
 };
+
 class Ticket {
 private:
 	ticketPlace tPlace;
@@ -34,9 +81,34 @@ private:
 	
 
 public:
+	
+	void settPlace(ticketPlace tPlace) {
+		this->tPlace = tPlace;
+	}
+	ticketPlace gettPlace() {
+		return tPlace;
+	}
+	void settType(ticketType tType) {
+		this->tType = tType;
+	}
+	ticketType gettType() {
+		return tType;
+	}
+	void setTicketId(string ticketId) {
+		this->ticketId = ticketId;
+	}
+	string getTicketId() {
+		return ticketId;
+	}
+
 };
 
+class Billet {
+	Ticket* ticket;
+	Event* event;
+	Location* location;
+};
 
 int main(int argc, char* argv[]) {
-	
+	Billet bilet = Billet();
 }
